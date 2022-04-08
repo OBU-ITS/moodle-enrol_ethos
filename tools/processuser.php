f52b9c6e3be36bbda0289ff0ac67b15ab789ab87
Joe Souch <p0091841@brookes.ac.uk>
2022-04-08 13:19:25 +0100

    Update process user deprecated functions

--- tools/processuser.php
+++ tools/processuser.php
@@ -43,13 +43,13 @@
     // These columns are always shown in the users list.
     $requiredcolumns = array('city', 'country', 'lastaccess');
     // Extra columns containing the extra user fields, excluding the required columns (city and country, to be specific).
-    $extracolumns = get_extra_user_fields($context, $requiredcolumns);
+    $extracolumns = \core_user\fields::get_extra_user_fields($context, $requiredcolumns);
     // Get all user name fields as an array.
-    $allusernamefields = get_all_user_name_fields(false, null, null, null, true);
+    $allusernamefields = \core_user\fields::get_all_user_name_fields(false, null, null, null, true);
     $columns = array_merge($allusernamefields, $extracolumns, $requiredcolumns);
 
     foreach ($columns as $column) {
-        $string[$column] = get_user_field_name($column);
+        $string[$column] = \core_user\fields::get_display_name($column);
         if ($sort != $column) {
             $columnicon = "";
             if ($column == "lastaccess") {
