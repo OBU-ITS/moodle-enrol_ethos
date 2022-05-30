3db89841f265e250c731bd81ad4fdd7fbff090ca
Joe Souch <p0091841@brookes.ac.uk>
2022-05-30 10:27:13 +0100

    Update version, ids and max processing for test

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ require_once($CFG->libdir.'/weblib.php');
 $trace = new html_progress_trace();
 $processingService = new processing_service($trace);
 $lastProcessedID = 0;
-$maxProcessedID = 0;
+$maxProcessedID = 16929;
 $processingService->process_ethos_updates($lastProcessedID, $maxProcessedID);
 
 
