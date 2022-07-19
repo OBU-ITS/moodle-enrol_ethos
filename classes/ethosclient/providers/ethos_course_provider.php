890f6f542198dd0e7ddba6ea2348f3dfe9373931
Jock Coats <p0071665@brookes.ac.uk>
2022-07-19 09:25:16 +0100

    Jock - Course provider

--- classes/ethosclient/providers/ethos_course_provider.php
+++ classes/ethosclient/providers/ethos_course_provider.php
@@ -1,13 +1,15 @@
 <?php
 namespace enrol_ethos\ethosclient\providers;
 
+use enrol_ethos\ethosclient\entities\ethos_course_info;
 use enrol_ethos\ethosclient\providers\base\ethos_provider;
 
 class ethos_course_provider extends ethos_provider
 {
     private function __construct()
     {
-        // TODO
+        parent::__construct();
+        $this->prepareProvider('courses', 'v16');
     }
 
     private static ?ethos_course_provider $instance = null;
@@ -20,4 +22,22 @@ class ethos_course_provider extends ethos_provider
 
         return self::$instance;
     }
+
+    public function get($id) : ?ethos_course_info {
+        $item = $this->getFromEthosById($id);
+
+        return $this->convertToCourse($item);
+    }
+
+    public function getAll() : array {
+        $items = $this->getFromEthos();
+
+        return array_map('convertToCourse', $items);
+    }
+
+    private function convertToCourse(object $item) : ?ethos_course_info {
+        return new ethos_course_info();
+    }
+
+
 }
