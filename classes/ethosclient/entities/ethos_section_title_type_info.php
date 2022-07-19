d6d9dfe38700fe3c8b38b22c17bee0cccae19620
Jock Coats <p0071665@brookes.ac.uk>
2022-07-19 11:46:01 +0100

    Jock - ethos_section_title_type_provider/_info

--- classes/ethosclient/entities/ethos_section_title_type_info.php
+++ classes/ethosclient/entities/ethos_section_title_type_info.php
@@ -4,5 +4,23 @@ namespace enrol_ethos\ethosclient\entities;
 
 class ethos_section_title_type_info
 {
+    public function __construct(object $data)
+    {
+        $this->populateObject($data);
+    }
+
+    public string $id;
+    public string $title;
+    public string $code;
+
+    public function populateObject(object $data) {
+        if(!isset($data)) {
+            return;
+        }
+
+        $this->id = $data->id;
+        $this->code = $data->code;
+        $this->title = $data->title;
+     }
 
 }
