42c927e7e7e6410b080d675d7e9d9b024131cce2
Jock Coats <p0071665@brookes.ac.uk>
2022-07-19 13:45:21 +0100

    Jock - educational_institution_unit_info and _provider

--- classes/ethosclient/entities/ethos_educational_institution_unit_info.php
+++ classes/ethosclient/entities/ethos_educational_institution_unit_info.php
@@ -4,5 +4,23 @@ namespace enrol_ethos\ethosclient\entities;
 
 class ethos_educational_institution_unit_info
 {
+    public function __construct(object $data)
+    {
+        $this->populateObject($data);
+    }
+
+    public string $id;
+    public string $title;
+    public string $type;
+
+    public function populateObject(object $data) {
+        if(!isset($data)) {
+            return;
+        }
+
+        $this->id = $data->id;
+        $this->type = $data->type;
+        $this->title = $data->title;
+    }
 
 }
