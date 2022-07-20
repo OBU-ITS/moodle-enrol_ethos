c6b64a4b9b983580717a5773206941ca79592e0f
Jock Coats <p0071665@brookes.ac.uk>
2022-07-20 09:54:16 +0100

    Jock - error (;) in educational_unit_info

--- classes/ethosclient/entities/ethos_educational_institution_unit_info.php
+++ classes/ethosclient/entities/ethos_educational_institution_unit_info.php
@@ -10,7 +10,7 @@ class ethos_educational_institution_unit_info
     }
 
     public string $id;
-    public string $code
+    public string $code;
     public string $title;
     public string $type;
 
