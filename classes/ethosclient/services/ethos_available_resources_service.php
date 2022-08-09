87a8f1dca759c79bbedf261d457263a2db553285
EmirK224 <p0092396@brookes.ac.uk>
2022-08-09 17:51:29 +0100

    creating ethos_available_resources_service

--- classes/ethosclient/services/ethos_available_resources_service.php
+++ classes/ethosclient/services/ethos_available_resources_service.php
@@ -22,8 +22,24 @@ class ethos_available_resources_service {
         return self::$instance;
     }
 
-    public function consumeMessages($lastProcessedId = 0, $consumeLimit = 200) {
-        $url = ethos_client::API_URL . "/consume?limit=". $consumeLimit ."&lastProcessedID=" . $lastProcessedId;
-        return $this->ethosClient->getJson($url, null);
+    //public function consumeMessages($lastProcessedId = 0, $consumeLimit = 200) {
+    //    $url = ethos_client::API_URL . "/consume?limit=". $consumeLimit ."&lastProcessedID=" . $lastProcessedId;
+    //    return $this->ethosClient->getJson($url, null);
+    //}
+
+    public function getavailableresources() {
+        $url = ethos_client::API_URL . "/admin/available-resources";
+        $availableresources = $this->ethosClient->getJson($url, null);
+
+        foreach ($availableresources as $availableresource){
+            $resourceslist = $availableresource->resources;
+            foreach ($resourceslist as $Resource){
+                if ($Resource->representations->version === "v8"){
+                    echo ("found one!");
+                }
+            }
+        }
     }
+
+
 }
