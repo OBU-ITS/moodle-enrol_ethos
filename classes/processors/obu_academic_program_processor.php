64476960b2cdf08f84d3fdeafe0028ab17fd9a56
EmirK224 <p0092396@brookes.ac.uk>
2022-08-15 12:39:41 +0100

    creating academic_program_processor, person_processor, advisor_type_info+provider and student_advisor_relationships_info+provider

--- classes/processors/obu_academic_program_processor.php
+++ classes/processors/obu_academic_program_processor.php
@@ -3,12 +3,25 @@ namespace enrol_ethos\processors;
 
 use enrol_ethos\ethosclient\entities\consume\ethos_notification;
 use enrol_ethos\processors\base\obu_processor;
+use enrol_ethos\services\sync\obu_sync_academic_program_service;
+use progress_trace;
 
 class obu_academic_program_processor implements obu_processor {
     const RESOURCE_NAME = "academic-programs";
 
+    private obu_sync_academic_program_service $syncService;
+
+    private progress_trace $trace;
+
+    public function __construct($trace)
+    {
+        $this->syncService = obu_sync_academic_program_service::getInstance();
+
+        $this->trace = $trace;
+    }
+
     function process(ethos_notification $message)
     {
-        // TODO: Implement process() method.
+        $this->syncService->sync($this->trace, $message->id);
     }
 }
