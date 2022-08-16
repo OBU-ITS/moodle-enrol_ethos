0481654ab0b53dd23d9a9bde0f931fc18d68b773
Joe Souch <p0091841@brookes.ac.uk>
2022-08-16 11:54:14 +0100

    Create the advisor and instructor processors.

--- classes/services/sync/obu_sync_section_instructor_service.php
+++ classes/services/sync/obu_sync_section_instructor_service.php
@@ -2,12 +2,23 @@
 
 namespace enrol_ethos\services\sync;
 
+use enrol_ethos\entities\obu_users_info;
+use enrol_ethos\ethosclient\providers\ethos_section_instructors_provider;
+use enrol_ethos\services\moodle\mdl_user_service;
+use enrol_ethos\services\obu_staff_service;
 use progress_trace;
 
 class obu_sync_section_instructor_service
 {
+    private ethos_section_instructors_provider $sectionInstructorProvider;
+    private obu_staff_service $staffService;
+    private mdl_user_service $userService;
+
     private function __construct()
     {
+        $this->sectionInstructorProvider = ethos_section_instructors_provider::getInstance();
+        $this->staffService = obu_staff_service::getInstance();
+        $this->userService = mdl_user_service::getInstance();
     }
 
     private static ?obu_sync_section_instructor_service $instance = null;
@@ -22,8 +33,11 @@ class obu_sync_section_instructor_service
 
     public function sync(progress_trace $trace, $id)
     {
-        // get person (instructor) from the section_instructor record
+        $ethosSectionInstructor = $this->sectionInstructorProvider->get($id);
 
-        // update record
+        $users = new obu_users_info();
+        $this->staffService->get($users, $ethosSectionInstructor->getInstructor());
+
+        $this->userService->handleUserCreation($trace, $users);
     }
 }
--- classes/services/sync/obu_sync_student_advisor_service.php
+++ classes/services/sync/obu_sync_student_advisor_service.php
@@ -1,12 +1,26 @@
 <?php
 namespace enrol_ethos\services\sync;
 
+use enrol_ethos\entities\obu_users_info;
+use enrol_ethos\ethosclient\providers\ethos_student_advisor_relationship_provider;
+use enrol_ethos\services\moodle\mdl_user_service;
+use enrol_ethos\services\obu_staff_service;
+use enrol_ethos\services\obu_student_service;
 use progress_trace;
 
 class obu_sync_student_advisor_service
 {
+    private ethos_student_advisor_relationship_provider $studentAdvisorProvider;
+    private obu_staff_service $staffService;
+    private obu_student_service $studentService;
+    private mdl_user_service $userService;
+
     private function __construct()
     {
+        $this->studentAdvisorProvider = ethos_student_advisor_relationship_provider::getInstance();
+        $this->staffService = obu_staff_service::getInstance();
+        $this->studentService = obu_student_service::getInstance();
+        $this->userService = mdl_user_service::getInstance();
     }
 
     private static ?obu_sync_student_advisor_service $instance = null;
@@ -21,9 +35,12 @@ class obu_sync_student_advisor_service
 
     public function sync(progress_trace $trace, $id)
     {
-        // Get person (student) from student_advisor
-        // Get person (advisor) from student_advisor
+        $ethosStudentAdvisor = $this->studentAdvisorProvider->get($id);
 
-        // update both records
+        $users = new obu_users_info();
+        $this->staffService->get($users, $ethosStudentAdvisor->getAdvisor());
+        $this->studentService->get($users, $ethosStudentAdvisor->getStudent());
+
+        $this->userService->handleUserCreation($trace, $users);
     }
 }
