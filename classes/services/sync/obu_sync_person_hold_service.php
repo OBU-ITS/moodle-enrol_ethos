3d03722c20b7e63aa73517e1b8c29a7f641caeb3
Joe Souch <p0091841@brookes.ac.uk>
2022-09-12 11:49:05 +0100

    Update obu synce person holds service

--- classes/services/sync/obu_sync_person_hold_service.php
+++ classes/services/sync/obu_sync_person_hold_service.php
@@ -2,6 +2,7 @@
 namespace enrol_ethos\services\sync;
 
 use enrol_ethos\ethosclient\providers\ethos_person_hold_provider;
+use enrol_ethos\repositories\db_user_repository;
 use obu_person_hold_service;
 use progress_trace;
 
@@ -9,11 +10,15 @@ class obu_sync_person_hold_service
 {
     private ethos_person_hold_provider $personHoldProvider;
     private obu_person_hold_service $personHoldService;
+    private db_user_repository $userRepo;
 
     private function __construct()
     {
+        global $DB;
+
         $this->personHoldProvider = ethos_person_hold_provider::getInstance();
         $this->personHoldService = obu_person_hold_service::getInstance();
+        $this->userRepo = new db_user_repository($DB);
     }
 
     private static ?obu_sync_person_hold_service $instance = null;
@@ -33,14 +38,22 @@ class obu_sync_person_hold_service
             return;
         }
 
-        // get the Moodle user record
+        $user = $this->userRepo->getUserWhereProfileFieldEquals("person_guid", $hold->getPersonId());
+        if($user == null) {
+            $trace->output("User with hold ($id) not found.");
+            return;
+        }
 
-        // update the person hold profile field.
+        $this->personHoldService->remove($hold, $user);
     }
 
     public function remove(progress_trace $trace, string $id) {
-        // find user where profile field contains $id
+        $user = $this->userRepo->getUserWhereProfileFieldContains("person_holds", $id);
+        if($user == null) {
+            $trace->output("User with hold ($id) not found.");
+            return;
+        }
 
-        // remove hold from person
+        $this->personHoldService->remove($id, $user);
     }
 }
