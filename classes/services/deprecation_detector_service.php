047dce1aded3c320a92a8038cf9aac256cd04f24
EmirK224 <p0092396@brookes.ac.uk>
2022-09-27 17:06:13 +0100

    removing trace from dep detect service

--- classes/services/deprecation_detector_service.php
+++ classes/services/deprecation_detector_service.php
@@ -4,22 +4,18 @@ namespace enrol_ethos\services;
 use enrol_ethos\ethosclient\services\ethos_available_resources_service;
 use enrol_ethos\helpers\core_class_finder_helper;
 use enrol_ethos\ethosclient\providers\base\ethos_provider;
-use progress_trace;
 
 class deprecation_detector_service {
 
     private ethos_available_resources_service $availableResourcesService;
 
-    private progress_trace $trace;
-
     public array $providers;
 
-    private function __construct($trace)
+    private function __construct()
     {
         $this->availableResourcesService = ethos_available_resources_service::getInstance();
         $this->populateProviders();
         $this->getRelevantAvailableResources($this->providers);
-        $this->trace = $trace;
     }
 
     private static ?deprecation_detector_service $instance = null;
@@ -44,7 +40,7 @@ class deprecation_detector_service {
                 continue;
             }
 
-            $instance = new $class($this->trace);
+            $instance = new $class();
             if ($instance instanceof ethos_provider) {
                 $resourceName = constant($class::RESOURCE_NAME);
                 $this->providers[$resourceName] = $instance;
