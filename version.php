321187aa09eb399bfe3bffc98f29dbdf8e81438f
Joe Souch <p0091841@brookes.ac.uk>
2025-01-17 14:07:54 +0000

    VErsion bmp

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2024112601;        // The current plugin version (Date: YYYYMMDDRR)
+$plugin->version   = 2025011701;        // The current plugin version (Date: YYYYMMDDRR)
 $plugin->requires  = 2018112800;        // Requires this Moodle version
 $plugin->component = 'enrol_ethos';  // Full name of the plugin (used for diagnostics)
-$plugin->release = '0.2.0';
+$plugin->release = '0.3.0';
